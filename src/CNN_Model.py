a5a4273c5e931d3c5823e1602751b5a9fc7496ab
Akondi Sai Karthik Vyas <vyasakondi009@gmail.com>
2023-11-16 15:31:11 -0500

    Deleted unwanted files

--- src/CNN_Model.py
+++ src/CNN_Model.py
@@ -5,7 +5,7 @@ class CNN_Model(tf.keras.Model):
     def __init__(self):
         super(CNN_Model, self).__init__()
         self.encoder = tf.keras.Sequential([
-            layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
+            layers.Conv2D(64, (3, 3), activation='relu', padding='same', input_shape=(128, 128, 3)),  # Example input shape
             layers.MaxPooling2D((2, 2), padding='same'),
             layers.Conv2D(128, (3, 3), activation='relu', padding='same'),
             layers.MaxPooling2D((2, 2), padding='same')
@@ -14,7 +14,7 @@ class CNN_Model(tf.keras.Model):
         self.decoder = tf.keras.Sequential([
             layers.Conv2DTranspose(128, (3, 3), strides=2, activation='relu', padding='same'),
             layers.Conv2DTranspose(64, (3, 3), strides=2, activation='relu', padding='same'),
-            layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same') 
+            layers.Conv2D(3, (3, 3), activation='sigmoid', padding='same')  # Adjusted for RGB output
         ])
     
     def call(self, x):
@@ -25,4 +25,3 @@ class CNN_Model(tf.keras.Model):
 model = CNN_Model()
 
 model.compile(optimizer='adam', loss='mean_squared_error')
-
